9109b4ab1f7053fc2e2509191a6898dd4a45020c
stevenchien123 <88325517+stevenchien123@users.noreply.github.com>
2022-02-03 12:36:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function OPEN() {
+    if (client.readyState === OPEN) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+export { Server, OPEN };
+
+import { Server, OPEN } from 'ws';
+
+const server = new Server({ port: 8080 });
+
+server.on('open', function open() {
+    console.log('connected');
+});
+
+server.on('close', function close() {
+    console.log('disconnected');
+});
+
+server.on('connection', function connection(ws, req) {
+    const ip = req.connection.remoteAddress;
+    const port = req.connection.remotePort;
+    const clientName = ip + port;
+
+    console.log('%s is connected', clientName)
+
+    ws.send("Welcome " + clientName);
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s from %s', message, clientName);
+
+        server.clients.forEach(function each(client) {
+            if (client.readyState === OPEN) {
+                client.send(clientName + " -> " + message);
+            }
+        });
+
+    });
+
+});
